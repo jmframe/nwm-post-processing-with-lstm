fb4d5d1c896062c6ce80351996985b4ed5e94d93
Jonathan Frame <jframe@adaptlogin101.nccs.nasa.gov>
2020-06-23 23:35:46 -0400

    igs on adapt

--- run_scripts/run_igs.sh
+++ run_scripts/run_igs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#SBATCH -t 12:00:00
+#SBATCH -p gpu
+#SBATCH -n1
+#SBATCH -G1
+#SBATCH -c1
+#SBATCH -J igs
+#SBATCH -o igs.out
+#SBATCH -e igs.error
 module purge
-source /home/jmframe/programs/anaconda3/etc/profile.d/conda.sh
+source /att/nobackup/jmframe/anaconda3/etc/profile.d/conda.sh
 conda activate pytorch
-cd /home/NearingLab/projects/jmframe/lstm_camels/
+cd /att/nobackup/jmframe/lstm_camels/
 echo 'calculating integrated gradients'
-
-python3 -u ig_nwm_only.py &> igs.out &
+python -u ig_nwm_only.py
